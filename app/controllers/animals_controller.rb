a8db1752310a5bbc220aa56808daa532f72122cf
Janice Huang <janicehuang@gmail.com>
2019-10-06 20:40:24 -0700

    start making new animal functionality

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -12,4 +12,24 @@ class AnimalsController < ApplicationController
       return
     end
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animal_path(@animal)
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/animals', to: 'animals#index', as: :animals
+  get '/animals/new', to: 'animals#new', as: :new_animal
   get '/animals/:id', to: 'animals#show', as: :animal
-  get '/animals/new', to: 'animals#new'
-  patch '/animals/', to: 'animals#update'
+  post '/animals', to: 'animals#create'
 end
+  # patch '/animals/', to: 'animals#update', as: :new_animal
+
